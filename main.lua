8dc52e554d7c81a3a19bd1706d6ceb7f14300d9c
crubz11 <max@kaiser.family>
2024-10-18 10:59:50 +0200

    Finished Player.lua Code

--- main.lua
+++ main.lua
@@ -8,13 +8,29 @@ function love.load()
 
 	local show_debugging = true
 
-	player = Player(show_debugging)
+	_G.player = Player(show_debugging)
+end
+
+function love.keypressed(key)
+	if key == "w" or key == "up" or key == "kp8" then
+		player.thrusting = true
+	end
+end
+function love.keyreleased(key)
+	if key == "w" or key == "up" or key == "kp8" then
+		player.thrusting = false
+	end
 end
 
 function love.update(dt)
 	_G.mouse_x, _G.mouse_y = love.mouse.getPosition()
+	player:movePlayer()
 end
 
 function love.draw()
 	player:draw()
+
+	love.graphics.setColor(1, 1, 1, 1)
+
+	love.graphics.print(love.timer.getFPS(), 10, 10)
 end
