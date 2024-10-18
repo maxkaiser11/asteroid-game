91effef38e43f3b8e3153489dffdd315a890e3a3
crubz11 <max@kaiser.family>
2024-10-18 10:46:10 +0200

    main.lua

---
+++ main.lua
@@ -0,0 +1,20 @@
+local love = require("love")
+
+local Player = require("Player")
+
+function love.load()
+	love.mouse.setVisible(false)
+	_G.mouse_x, _G.mouse_y = 0, 0
+
+	local show_debugging = true
+
+	player = Player(show_debugging)
+end
+
+function love.update(dt)
+	_G.mouse_x, _G.mouse_y = love.mouse.getPosition()
+end
+
+function love.draw()
+	player:draw()
+end
